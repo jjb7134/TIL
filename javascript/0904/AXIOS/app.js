4c422ffa27cbc07262aceebdf59587a7859f9fde
firely95 <90025832+jjb7134@users.noreply.github.com>
2022-09-05 00:47:26 +0900

    AJAX,API

---
+++ javascript/0904/AXIOS/app.js
@@ -0,0 +1,29 @@
+// axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
+
+const jokes = document.querySelector('#jokes');
+const button = document.querySelector('button');
+
+const addNewJoke = async () => {
+    const jokeText = await getDadJoke();
+    const newLI = document.createElement('LI');
+    newLI.append(jokeText);
+    jokes.append(newLI)
+}
+
+const getDadJoke = async () => {
+    // const config = { headers: { Accept: 'application/json' } };
+    // const res = await axios.get('https://icanhazdadjoke.com/api', config);
+    // return res.data.joke;
+    // const newLI = document.createElement('LI');
+    // newLI.append(res.data.joke);
+    // jokes.append(newLI);
+    try {
+        const config = { headers: { Accept: 'application/json' } }
+        const res = await axios.get('https://icanhazdadjoke.com/', config)
+        return res.data.joke;
+    } catch (e) {
+        return "NO JOKES AVAILABLE! SORRY :("
+    }
+}
+
+button.addEventListener('click', addNewJoke)
