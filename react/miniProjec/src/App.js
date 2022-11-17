40c8949d73ca1fca90f8ddf2c35683d49ed73697
정종빈 <90025832+jjb7134@users.noreply.github.com>
2022-11-17 14:41:43 +0900

    state, 컴포넌트 추가

--- react/miniProjec/src/App.js
+++ react/miniProjec/src/App.js
@@ -1,11 +1,49 @@
-import React from 'react';
-import AddUser from './components/Users/AddUser';
+// import React,{useState} from 'react';
+// import AddUser from './components/Users/AddUser';
+// import UserList from './components/Users/UsersList';
 
+
+// function App() {
+//   const [usersList, setUsersList] = useState([]);
+
+//   const addUserHandler = (uName, uAge) => {
+//     setUsersList((prev) => {
+//       return [...prev, {name: uName, age: uAge, id:Math.random().toString() },];
+//     });
+//   };
+
+
+//   return (
+//     <div>
+//       <AddUser onAddUser={addUserHandler}/>
+//       <UserList users={[usersList]}/>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useState } from 'react';
+
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
